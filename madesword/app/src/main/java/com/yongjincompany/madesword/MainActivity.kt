a02d35c62e8829370e1d1a3a90dab41f7b9d07c4
Leesin0222 <dragon02229@naver.com>
2021-12-29 15:08:22 +0900

    📑 :: 플로팅액션바 애니메이션 구현

--- madesword/app/src/main/java/com/yongjincompany/madesword/MainActivity.kt
+++ madesword/app/src/main/java/com/yongjincompany/madesword/MainActivity.kt
@@ -2,11 +2,70 @@ package com.yongjincompany.madesword
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import com.yongjincompany.madesword.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val rotateOpen: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_open_anim)}
+    private val rotateClose: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_close_anim)}
+    private val fromBottom: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.from_bottom_anim)}
+    private val toBottom: Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.to_bottom_anim)}
+
+    private lateinit var binding:ActivityMainBinding
+
+    private var clicked = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_Madesword)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.menuBtn.setOnClickListener {
+            onMenuButtonClicked()
+        }
+
+        binding.shop.setOnClickListener {
+
+        }
+
+        binding.setting.setOnClickListener {
+
+        }
+    }
+
+
+
+
+    private fun onMenuButtonClicked() {
+        setVisibility(clicked)
+        setAnimation(clicked)
+        clicked = !clicked
+    }
+
+
+    private fun setAnimation(clicked: Boolean){
+        if(!clicked){
+            binding.setting.visibility = View.VISIBLE
+            binding.shop.visibility = View.VISIBLE
+        }else{
+            binding.setting.visibility = View.INVISIBLE
+            binding.shop.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun setVisibility(clicked: Boolean) {
+        if(!clicked){
+            binding.setting.startAnimation(fromBottom)
+            binding.shop.startAnimation(fromBottom)
+            binding.menuBtn.startAnimation(rotateOpen)
+        }else{
+            binding.setting.startAnimation(toBottom)
+            binding.shop.startAnimation(toBottom)
+            binding.menuBtn.startAnimation(rotateClose)
+        }
     }
 }
