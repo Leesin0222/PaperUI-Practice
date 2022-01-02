38ac7d8e30349ad0a97942b2e94244110c1ca10d
Leesin0222 <dragon02229@naver.com>
2022-01-03 00:45:48 +0900

    📑 :: RetrofitClient 틀

--- madesword/app/src/main/java/com/yongjincompany/madesword/retrofit/RetrofitClient.kt
+++ madesword/app/src/main/java/com/yongjincompany/madesword/retrofit/RetrofitClient.kt
@@ -1,4 +1,25 @@
 package com.yongjincompany.madesword.retrofit
 
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object RetrofitClient {
+    const val MainServer = "http://api.drfriday.in/api/user/"
+
+    const val ImageMainServer = "http://api.drfriday.in/";
+
+    val retrofitClient: Retrofit.Builder by lazy {
+
+        Retrofit.Builder()
+            .baseUrl("http://")
+            .client(OkHttpClient.Builder().build())
+            .addConverterFactory(GsonConverterFactory.create())
+    }
+
+    val apiInterface: ApiInterface by lazy {
+        retrofitClient
+            .build()
+            .create(ApiInterface::class.java)
+    }
 }
